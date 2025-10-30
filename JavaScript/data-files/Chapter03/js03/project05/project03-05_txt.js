a9c27d1f29039efbfa113d4c59f742eb002a9f5a
Joshua-Arellano <83684718+Joshua-Arellano@users.noreply.github.com>
2025-10-30 15:40:24 -0500

    chapter 3 project 5

--- JavaScript/data-files/Chapter03/js03/project05/project03-05_txt.js
+++ JavaScript/data-files/Chapter03/js03/project05/project03-05_txt.js
@@ -19,26 +19,25 @@ let sales = [10281, 12255, 25718, 21403, 16142];
 let totalSales = 0;
 
 // Use the forEach() method to sum the sales for each phone model and add it to the totalSales variable
-sales.forEach(addtoTotal);
+sales.forEach(addToTotal);
 
 // For loop to generate bar chart of phone sales
 for (let i = 1; i <= phones.length; i++) {
-   
-   let barChart = "";   // Variable to store HTML code for table cells used to create bar chart
-   
+
+   let barChart = "<tr>";   // Variable to store HTML code for table cells used to create bar chart
+
    // Calculate the percent of total sales for a particular phone model
-   let barPercent = 100*sales/totalSales;
-   
-   
+   let barPercent = 100*sales[i]/totalSales;
+
    let cellTag;   // Variable that will define the class of td elements used in creating the bar chart cells.
    
    // Determine the table cell based on the value of the phone model
    switch (phones[i]) {
-      case "Photon 6E"  cellTag = "<td class='group1'>"; break;
-      case "Photon 6X"  cellTag = "<td class='group2'>"; break;
-      case "Photon 7E"  cellTag = "<td class='group3'>"; break;
-      case "Photon 7X"  cellTag = "<td class='group4'>"; break;
-      case "Photon 8P"  cellTag = "<td class='group5'>"; break;         
+      case "Photon 6E":  cellTag = "<td class='group1'>"; break;
+      case "Photon 6X":  cellTag = "<td class='group2'>"; break;
+      case "Photon 7E":  cellTag = "<td class='group3'>"; break;
+      case "Photon 7X":  cellTag = "<td class='group4'>"; break;
+      case "Photon 8P":  cellTag = "<td class='group5'>"; break;         
    }
    
    // Table cell containing information on phone and total sales (formatted using a thousands-separator)
@@ -50,8 +49,8 @@ for (let i = 1; i <= phones.length; i++) {
    }
    barChart += "</tr>";
 
-   // Insert the bChart HTML code into first tbody element in the web document
-   document.getElementsByTagName("tbody").insertAdjacentHTML("beforeEnd", barChart);
+   // Insert the barChart HTML code into first tbody element in the web document
+   document.getElementsByTagName("tbody")[0].insertAdjacentHTML("beforeEnd", barChart);
 }
 
 
