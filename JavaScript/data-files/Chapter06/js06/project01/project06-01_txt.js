4819702a1f95afdbb22d48fabad016772477857d
Joshua-Arellano <83684718+Joshua-Arellano@users.noreply.github.com>
2025-11-17 08:03:41 -0600

    Ch 6 Project 1

--- JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
+++ JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
@@ -4,9 +4,22 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Joshua Arellano
+      Date: 11/15/2025
 
       Filename: project06-01.js
 */
 
+const submitButton = document.getElementById("submitButton");
+let pwd = document.getElementById("pwd");
+let pwd2 = document.getElementById("pwd2");
+
+submitButton.addEventListener("click", function() {
+      if (pwd.value.length < 8 || !/[A-Za-z]/.test(pwd.value) || !/\d/.test(pwd.value)) {
+            alert("Your password must be at least 8 characters with at least one letter and one number");
+      } else if (pwd.value !== pwd2.value) {
+            alert("Your passwords do not match");
+      } else {
+            ""
+      }
+});
