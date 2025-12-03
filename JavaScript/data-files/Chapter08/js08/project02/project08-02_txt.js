3d4f28b3d0321ae4ba0d20cf48b6432d3d9a0a48
Joshua-Arellano <83684718+Joshua-Arellano@users.noreply.github.com>
2025-12-02 20:28:51 -0600

    Ch 8 project 1 + 2

--- JavaScript/data-files/Chapter08/js08/project02/project08-02_txt.js
+++ JavaScript/data-files/Chapter08/js08/project02/project08-02_txt.js
@@ -4,8 +4,8 @@
       Project 08-02
 
       Project to add balls bouncing within a container
-      Author: 
-      Date:   
+      Author: Joshua Arellano
+      Date: 12/2/2025
 
       Filename: project08-02.js
 */
@@ -17,11 +17,38 @@ const BOX_WIDTH = 800;  // width of the container in pixels
 
 /*--------------- Object Code --------------------*/
 
+let box = {
+   width: BOX_WIDTH,
+   height: BOX_HEIGHT,
+   xPos: 0,
+   yPos: 0
+};
 
+let ball = function(size) {
+   this.radius = size;
+   this.xPos = null;
+   this.yPos = null;
+   this.xVelocity = null;
+   this.yVelocity = null;
+}
 
+ball.prototype.moveWithin = function(container) {
+   let ballTop = this.yPos;
+   let ballLeft = this.xPos;
+   let ballBottom = this.yPos + this.radius;
+   let ballRight = this.xPos + this.radius;
 
+   if (ballTop < 0 || ballBottom > container.height) {
+      container.yPos += this.yVelocity;
+      this.yVelocity = -this.yVelocity;
+   } if (ballLeft < 0 || ballRight > container.width) {
+      container.xPos += this.xVelocity;
+      this.xVelocity = -this.xVelocity;
+   }
 
-
+   this.yPos += this.yVelocity;
+   this.xPos += this.xVelocity;
+}
 
 /*---------------Interface Code -----------------*/
 
@@ -46,8 +73,21 @@ addBall.onclick = function() {
    // Append the ball image to the box
    boxImage.appendChild(ballImage);     
    
+   // Create instance of ball object
+   let newBall = new ball(BALL_RADIUS);
+   newBall.yPos = (BOX_HEIGHT - BALL_RADIUS)/2;
+   newBall.xPos = (BOX_WIDTH - BALL_RADIUS)/2;
+   newBall.yVelocity = rand(-10, 10);
+   newBall.xVelocity = rand(-10, 10);
    
-   
+   // Animation
+   window.setInterval(function() {
+      newBall.moveWithin(box);
+      ballImage.style.top = newBall.yPos + "px";
+      ballImage.style.left = newBall.xPos + "px";
+      boxImage.style.top = box.yPos + "px";
+      boxImage.style.left = box.xPos + "px";
+   })
 };
 
 
